3e6f00fc8e0010b2075fc88b9889b6cafbaa6a85
Nicola Iarocci <nicola@nicolaiarocci.com>
2018-11-16 17:33:22 +0100

    Tests for IRepositoryExtensions class
    Addresses #7

---
+++ Core/Test/IRepositoryExtensionsTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using DataStorage.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class IRepositoryExtensionsTest
+    {
+        private Repository Repository;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            Repository = new Repository();
+        }
+
+        [TestMethod]
+        public void FindThrows()
+        {
+            Assert.ThrowsException<NotImplementedException>(() => Repository.Find<Class>(x => x.Name == "name"));
+        }
+    }
+}
