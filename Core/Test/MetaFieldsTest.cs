d23fa67916fdf0601d4dff588d1caa8f13a11126
Nicola Iarocci <nicola@nicolaiarocci.com>
2018-11-16 17:20:54 +0100

    Tests for Core.MetaFields class
    Addresses #7

---
+++ Core/Test/MetaFieldsTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using DataStorage.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class MetaFieldsTest
+    {
+        private MetaFields MetaFields;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            MetaFields = new MetaFields();
+        }
+
+        [TestMethod]
+        public void Defaults()
+        {
+            Assert.AreEqual("Id", MetaFields.Id);
+            Assert.AreEqual("LastUpdated", MetaFields.LastUpdated);
+        }
+
+        [TestMethod]
+        public void AsList()
+        {
+            var challenge = MetaFields.AsList();
+            Assert.IsTrue(challenge.Contains(MetaFields.Id));
+            Assert.IsTrue(challenge.Contains(MetaFields.LastUpdated));
+        }
+    }
+}
