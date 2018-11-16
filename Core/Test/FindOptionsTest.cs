6b6008104a012c7d904e736f9bf5a557114d62f3
Nicola Iarocci <nicola@nicolaiarocci.com>
2018-11-16 17:26:43 +0100

    Tests for Core.FindOptions class
    Addresses #7

---
+++ Core/Test/FindOptionsTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using DataStorage.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class FindOptionsTest
+    {
+        private FindOptions<Class> FindOptions;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            FindOptions = new FindOptions<Class>();
+        }
+
+        [TestMethod]
+        public void Defaults()
+        {
+            Assert.IsFalse(FindOptions.IfModifiedSince.HasValue);
+        }
+    }
+}
