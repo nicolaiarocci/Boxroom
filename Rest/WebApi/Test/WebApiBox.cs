5f12a23af174d201776aeaaee337c8541d11e3a1
Nicola Iarocci <nicola@nicolaiarocci.com>
2019-02-14 15:54:22 +0100

    test refactoring

--- Rest/WebApi/Test/WebApiBox.cs
+++ Rest/WebApi/Test/WebApiBox.cs
@@ -14,19 +14,17 @@ namespace Test
         }
 
         [Test]
-        public void RenderThrowsNotImplmented()
+        public void RenderAsQueryString()
         {
-            var updatedOn = DateTime.Now.Date;
-            var c = new Class { AProperty = "a property value" };
+            var aClassInstance = new Class { AProperty = "a property value" };
+            var aVariable = DateTime.Now.Date;
+
             var box = new WebApiBox();
 
-            var result = box.RenderAsQueryString<Class>(
-                // a constant value, needs escaping.
-                x => x.Name == "a name" &&
-                // class property value.
-                x.AProperty == c.AProperty &&
-                // field (variable) value.
-                x.UpdatedOn == updatedOn
+            var result = box.RenderAsQueryString<Class>(x =>
+               x.Name == "a name" &&
+               x.AProperty == aClassInstance.AProperty &&
+               x.UpdatedOn == aVariable
                 );
 
             Assert.AreEqual("?Name=a%20name&AProperty=a%20property%20value&UpdatedOn=Thu,%2014%20Feb%202019%2000:00:00%20GMT", result);
